809f130d08fc79f8c5d9e69c2f588efaa1574d63
Gaurav Saxena <32961153+gurvsxen487@users.noreply.github.com>
2019-06-12 12:07:25 +0530

    full signup functionality

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// Sign up validations
-var name = document.getElementById('signup-name');
+// Sign up validations--------------------------------------------
+var names = document.getElementById('signup-name');
 var email = document.getElementById('signup-email');
 var password = document.getElementById('signup-password');
-var msg = document.getElementById('message');
-password.addEventListener('blur',function(){
-    msg.style.display='none';
-});
-password.addEventListener('focus',function(){
-    msg.style.display='block';
-});
 var num = document.getElementById('num');
 var alpha = document.getElementById('alpha');
 var specialChar = document.getElementById('specialChar');
@@ -21,51 +14,106 @@ password.addEventListener('keyup',function(){
     // validate number
     var numPattern = /[0-9]/g;
     if(password.value.match(numPattern)){
-        num.classList.remove("invalid");
         num.classList.add("valid");
         tick1.style.display="inline";
         
     } else {
         num.classList.remove("valid");
-        num.classList.add('invalid');
         tick1.style.display="none";
 
     }
     // validate alphabets
     var letterPattern = /[a-zA-Z]/g;
     if(password.value.match(letterPattern)){
-        alpha.classList.remove('invalid');
         alpha.classList.add('valid');
         tick2.style.display="inline";
 
     } else {
         alpha.classList.remove('valid');
-        alpha.classList.add('invalid');
         tick2.style.display="none";
     }
     // validate special characters
     var specialPattern = /[^0-9a-zA-Z]/g;
     if(password.value.match(specialPattern)){
-        specialChar.classList.remove('invalid');
         specialChar.classList.add('valid');
         tick3.style.display="inline";
     } else {
         specialChar.classList.remove('valid');
-        specialChar.classList.add('invalid');
         tick3.style.display="none";
     }
     // validate length
     if(password.value.length>=8){
-        len.classList.remove('invalid');
         len.classList.add('valid');
         tick4.style.display="inline";
     } else {
         len.classList.remove('valid');
-        len.classList.add('invalid');
         tick4.style.display="none";
     }
     
-})
+});
+
+// validation of email ---------------------------------------------
+email.addEventListener('blur',function(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var emailError;
+
+    if (email.value=='') {
+        emailError = '';
+        document.getElementById('emailError').innerHTML=emailError;
+    } else if (!email.value.match(mailformat)){
+        emailError = 'Please enter a valid email id.';
+        document.getElementById('emailError').innerHTML=emailError;
+    } else {
+        emailError = '';
+        document.getElementById('emailError').innerHTML=emailError;
+    }
+});
+email.addEventListener('keyup',function(){
+    
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (email.value=='' || email.value.match(mailformat)){
+        emailError = '';
+        document.getElementById('emailError').innerHTML=emailError;
+    }    
+});
 
 
-// -----------------------------------
+// validation of name ----------------------------------------------
+names.addEventListener('blur',function(){
+    var nameformat = /^[a-zA-Z\s]*$/;
+    var nameError;
+    if (names.value=='') {
+        nameError = '';
+        document.getElementById('nameError').innerHTML=nameError;
+    } else if (!names.value.match(nameformat)){
+        nameError = 'Please enter a valid name.';
+        document.getElementById('nameError').innerHTML=nameError;
+    } else {
+        nameError = '';
+        document.getElementById('nameError').innerHTML=nameError;
+    }
+});
+names.addEventListener('keyup',function(){
+    var nameformat = /^[a-zA-Z\s]*$/;
+    var nameError;
+    if (names.value == '' || names.value.match(nameformat)) {
+        nameError = '';
+        document.getElementById('nameError').innerHTML = nameError;
+    }
+});
+
+
+// Store credentials ----------------------------------------------
+function storeCredentials(){
+    let userList = JSON.parse(localStorage.getItem('users')) || [];
+    var userObj = {
+        name: names.value,
+        email: email.value,
+        password: password.value
+    }
+    userList.push(userObj);
+    localStorage.setItem('users',JSON.stringify(userList));
+    singupSuccessfull = true;
+    $('#modalRegisterForm').modal('hide');
+    $('#signupSuccess').modal('show');
+};
